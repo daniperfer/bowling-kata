cd2027d5201025e788d6c512a52957912451c189
Daniel Pereira <depefe@gmail.com>
2018-04-07 13:21:08 +0200

    Take into account different types of bonus points. All tests pass OK.

--- bowling.py
+++ bowling.py
@@ -2,31 +2,41 @@ class Bowling:
     def __init__(self):
         self.overall = 0
         self.bonus = 0
+        # there might be bonus points from a frame before (strike or spare) and from two frames before (strike)
+        self.bonus_from_previous = 0
+        self.bonus_before_previous = 0
 
     def evaluate(self, game):
         for n_try, score in enumerate(game):
             if score == 'X':
                 self.overall += 10
-                if self.bonus > 0 and n_try < len(game)-2:
+                if self.bonus > 0:
                     self.overall += 10
                     self.bonus -= 1
-                if self.bonus > 0 and n_try < len(game)-2:
+                if self.bonus > 1:
                     self.overall += 10
                     self.bonus -= 1
-                if n_try < len(game)-2:
+                # strikes only give future bonus points if they occur before 10th frame
+                if n_try < len(game)-3:
                     self.bonus += 2
             if score == '/':
                 previous_points = int(game[n_try-1])
                 self.overall += (10 - previous_points)
-                print("n_try: {}, len(game):{}".format(n_try, len(game)))
-                if self.bonus > 0 and n_try < len(game)-2:
+                if self.bonus > 0:
                     self.overall += (10 - previous_points)
                     self.bonus -= 1
+                if self.bonus > 1:
+                    self.overall += (10 - previous_points)
+                    self.bonus -= 1
+                # spares only give future bonus points if they occur before 10th frame
                 if n_try < len(game)-2:
                     self.bonus += 1
             if score.isdigit():
                 self.overall += int(score)
-                if self.bonus > 0 and n_try < len(game)-2:
+                if self.bonus > 0:
+                    self.overall += int(score)
+                    self.bonus -= 1
+                if self.bonus > 1:
                     self.overall += int(score)
                     self.bonus -= 1
 
--- test_bowling.py
+++ test_bowling.py
@@ -52,6 +52,20 @@ class TestClass(unittest.TestCase):
         total_score = bowling_game.evaluate(game)
         self.assertEqual(total_score, game_score)
 
+    def test_case_nine_pins_in_all_frames(self):
+        game = "9-9-9-9-9-9-9-9-9-9-"
+        game_score = 10*9
+        bowling_game = Bowling()
+        total_score = bowling_game.evaluate(game)
+        self.assertEqual(total_score, game_score)
+
+    def test_case_spare_before_last_frame(self):
+        game = "9-9-9-9-9-9-9-9-9/9-"
+        game_score = 8*9 + 1*(10+9) + 1*9
+        bowling_game = Bowling()
+        total_score = bowling_game.evaluate(game)
+        self.assertEqual(total_score, game_score)
+
     def test_case_all_zeros(self):
         game = "--------------------"
         game_score = 0
@@ -69,6 +83,13 @@ class TestClass(unittest.TestCase):
     def test_case_all_strikes_in_single_frame(self):
         single_frame = "XXX"
         single_frame_score = 10 + 10 + 10
+        bowling_game = Bowling()
+        total_score = bowling_game.evaluate(single_frame)
+        self.assertEqual(total_score, single_frame_score)
+
+    def test_case_two_strikes_in_two_frames(self):
+        single_frame = "X17X44"
+        single_frame_score = 1*(10+1+7) + 1*(1+7) + 1*(10+4+4)
         bowling_game = Bowling()
         total_score = bowling_game.evaluate(single_frame)
         self.assertEqual(total_score, single_frame_score)
